8287c8924a05e588f23aac886352cc3e622617fe
unknown <2296510@mail.ru>
2022-09-13 19:04:40 +0500

    неужели, надо через фор еще попробовать, проверить как фор работает

---
+++ dz3primer3/Program.cs
@@ -0,0 +1,18 @@
+﻿//Задача 23
+//Напишите программу, которая принимает на вход 
+//число (N) и выдаёт таблицу кубов чисел от 1 до N.
+//3 -> 1, 8, 27
+//5 -> 1, 8, 27, 64, 125
+Console.Write("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+Console.Write($"{N} -> ");
+while(i < N)
+{ 
+    Console.Write($"{Math.Pow(i, 3)}, ");
+    i = i + 1;
+}
+if(i == N)
+{
+    Console.Write($"{Math.Pow(i, 3)}");
+}
