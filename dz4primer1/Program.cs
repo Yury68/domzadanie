ada5fcdbbb895509ea86d7f3151c4f0c116dc077
unknown <2296510@mail.ru>
2022-09-15 02:33:44 +0500

    даже через фор проперло

---
+++ dz4primer1/Program.cs
@@ -0,0 +1,22 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B)
+// и возводит число A в натуральную степень B.
+//3, 5 -> 243 (3⁵)
+//2, 4 -> 16
+//Console.OutputEncoding = System.Text.Encoding.Unicode;спросить у ребят (у Макса), 
+//какэто работает, конвертировать наверно по другому надо
+Console.Write("Введите число А: ");
+int A = int.Parse(Console.ReadLine());
+Console.Write("Введите число В: ");
+int B = int.Parse(Console.ReadLine());
+
+int Avstep(int x, int n)
+{
+    int res = 1;
+    for (int i = 1; i <= n; i++)
+    {
+         res = res * x;
+    }
+    return res;
+}
+Console.Write($"{A}, {B} -> {Avstep(A, B)}");//{A}{UpString(B)} цифру "В" должно в 
+//верхний регистр закинуть, типа степень показать
