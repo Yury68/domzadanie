273868857332112a0f117f8ca78d9c18ebad95ed
unknown <2296510@mail.ru>
2022-09-16 01:01:43 +0500

    да ужжж, 6 часов

---
+++ dz4primer2/Program.cs
@@ -0,0 +1,20 @@
+﻿//Задача 27: Напишите программу, которая принимает на вход число
+//и выдаёт сумму цифр в числе.
+//452 -> 11
+//82 -> 10
+//9012 -> 12
+Console.Clear();
+Console.Write("Введите число: ");
+int N= int.Parse(Console.ReadLine());
+int res = 0;
+
+int sum(int a)
+{
+    while(a > 0)    
+{
+    res = res + a % 10;
+    a = a / 10;
+}
+    return res;
+}
+    Console.WriteLine($"{N} -> {sum(N)}");
